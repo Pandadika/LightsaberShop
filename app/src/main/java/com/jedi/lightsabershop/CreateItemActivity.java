bd0ebddffeb6ea2aac9fc8eb2755fbd36a97c638
Pandadika <pandadika@gmail.com>
2024-09-10 20:35:39 +0200

    Revert "bunch of stuff"
    This reverts commit 2425d01baee857df58dd4ddd5d1ecc6958473eef.

--- app/src/main/java/com/jedi/lightsabershop/CreateItemActivity.java
+++ app/src/main/java/com/jedi/lightsabershop/CreateItemActivity.java
@@ -1,26 +1,11 @@
 package com.jedi.lightsabershop;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-
-import com.google.gson.Gson;
-import com.jedi.jedishared.Component;
-import com.jedi.jedishared.Item;
-
-import java.util.UUID;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class CreateItemActivity extends BaseActivity {
   
@@ -31,8 +16,6 @@ public class CreateItemActivity extends BaseActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ItemApi itemApi = retrofit.create(ItemApi.class);
-    
     setContentView(R.layout.activity_create_item);
     
     itemNameInput = findViewById(R.id.itemName);
@@ -41,7 +24,7 @@ public class CreateItemActivity extends BaseActivity {
     itemDescriptionInput = findViewById(R.id.itemDescription);
     createItemButton = findViewById(R.id.createItemButton);
     
-    itemComponentSpinner.setAdapter(new ComponentSpinnerAdapter(this, Component.values()));
+    itemComponentSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Component.getFormattedNames()));
     
     createItemButton.setOnClickListener(view -> {
       String name = itemNameInput.getText().toString();
@@ -49,43 +32,13 @@ public class CreateItemActivity extends BaseActivity {
       double price = Double.parseDouble(itemPriceInput.getText().toString());
       String description = itemDescriptionInput.getText().toString();
       
-      Item newItem = new Item(name, component, price, description); //TODO Save this :D
-      Gson gson = new Gson();
-      String json = gson.toJson(newItem);
-      
-      TextView textView = new TextView(CreateItemActivity.this);
-      textView.setText("No connection");
-      textView.setBackgroundResource(R.drawable.rounded_tost_background_fail);
-      Call<UUID> call = itemApi.createItem(newItem);
-          call.enqueue(new Callback<UUID>() {
-            @Override
-            public void onResponse(@NonNull Call<UUID> call, @NonNull Response<UUID> response) {
-              if (!response.isSuccessful()) {
-                textView.setText(newItem.getName() + response.code());
-                textView.setBackgroundResource(R.drawable.rounded_tost_background_fail);
-                return;
-              }
-              textView.setText(newItem.getName() + " saved!");
-              textView.setBackgroundResource(R.drawable.rounded_tost_background_success);
-            }
-            
-            @Override
-            public void onFailure(Call<UUID> call, Throwable t) {
-              textView.setText(newItem.getName() + " failed!");
-              textView.setBackgroundResource(R.drawable.rounded_tost_background_fail);
-            }
-          });
-
-      textView.setTextColor(Color.WHITE);
-      textView.setPadding(20, 20, 20, 20);
-      
-      Toast toast = new Toast(CreateItemActivity.this);
-      toast.setView(textView);
-      toast.setDuration(Toast.LENGTH_SHORT);
-      toast.setGravity(Gravity.TOP, 0, 100);
-      toast.show();
-      
+      Item newItem = new Item(name, component, price, description);
+      Toast.makeText(CreateItemActivity.this, "New component created!", Toast.LENGTH_SHORT).show();
       finish();
     });
   }
+/*    @Override
+protected int getLayoutResourceId() {
+    return R.layout.activity_main;
+}*/
 }
--- app/src/main/java/com/jedi/lightsabershop/MainActivity.java
+++ app/src/main/java/com/jedi/lightsabershop/MainActivity.java
@@ -2,11 +2,9 @@ package com.jedi.lightsabershop;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,24 +12,12 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.jedi.jedishared.Component;
-import com.jedi.jedishared.Item;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends BaseActivity {
   
   Button adminButton;
-  Button loginButton;
-  Button cartButton;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -53,51 +39,12 @@ public class MainActivity extends BaseActivity {
       startActivity(intent);
     });
     
-    loginButton = findViewById(R.id.loginpage);
-    loginButton.setOnClickListener(view -> {
-      Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-      startActivity(intent);
-    });
+    List<Item> items = new ArrayList<>(generateItems());
     
-    cartButton = findViewById(R.id.cartpage);
-    cartButton.setOnClickListener(view -> {
-      Intent intent = new Intent(MainActivity.this, CartActivity.class);
-      startActivity(intent);
-    });
-  }
-  
-  @Override
-  protected void onResume() {
-    super.onResume();
-    
-    ItemApi itemApi = retrofit.create(ItemApi.class);
-    Call<List<Item>> savedItems = itemApi.getItems();
-    
-    List<Item> items = new ArrayList<>();
-    savedItems.enqueue(new Callback<List<Item>>() {
-      @Override
-      public void onResponse(@NonNull Call<List<Item>> call, @NonNull Response<List<Item>> response) {
-        if (response.isSuccessful() && response.body() != null) {
-          items.clear(); // Clear existing items
-          for (Item item : response.body()) {
-            items.add(new Item(item.getName(), item.getComponent(), item.getPrice(), item.getDescription()));
-          }
-          runOnUiThread(() -> {
-            RecyclerView recyclerView = findViewById(R.id.recyclerView);
-            recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-            RecyclerView.Adapter<ItemAdapter.ViewHolder> itemAdapter = new ItemAdapter(items, MainActivity.this);
-            recyclerView.setAdapter(itemAdapter);
-          });
-        } else {
-          Log.e("MainActivity", "Error fetching items: API Response: " + response.message());
-        }
-      }
-      
-      @Override
-      public void onFailure(@NonNull Call<List<Item>> call, @NonNull Throwable t) {
-        Log.e("MainActivity", "Error fetching items: Throwable ", t);
-      }
-    });
+    RecyclerView recyclerView = findViewById(R.id.recyclerView);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    RecyclerView.Adapter<ItemAdapter.ViewHolder> ItemAdapter = new ItemAdapter(items, this);
+    recyclerView.setAdapter(ItemAdapter);
   }
   
   public static List<Item> generateItems() {
